f5f9dba62e99f746bdc78a9dfe58c0a3b2e61f41
Clark Chen <yyunqingchenn@gmail.com>
2019-07-08 01:30:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n){
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  
+  for (var i = 0; i<upRank.length; i++){
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n);
+  }
+}
+
+function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div');
+  return deep[deep.length-1];
+}
