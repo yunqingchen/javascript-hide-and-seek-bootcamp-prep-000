22e5cb51f8162ca5e1f4f60c9e547756bf2e2d6b
Clark Chen <yyunqingchenn@gmail.com>
2019-07-08 01:29:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.getElementbyId('nested').querySelector('div.target');
+  return document.getElementById('nested').querySelector('div.target');
 }
 
 function increaseRankBy(n){
-  const upRank = document.getElementbyId('app').querySelectorAll('ul.ranked-list');
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list');
   
   for (var i = 0; i<upRank.length; i++){
     upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n);
@@ -15,6 +15,6 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  var deep = document.getElementbyId('grand-node').querySelectorAll('div');
+  var deep = document.getElementById('grand-node').querySelectorAll('div');
   return deep[deep.length-1];
 }
