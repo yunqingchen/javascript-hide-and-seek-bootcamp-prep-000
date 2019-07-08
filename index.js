14ef508c5f4d59ada4fccdd778a4115c71c1c2de
Clark Chen <yyunqingchenn@gmail.com>
2019-07-08 01:25:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function increaseRankBy(n){
   const upRank = document.getElementbyId('app').querySelectorAll('ul.ranked-list');
   
   for (var i = 0; i<upRank.length; i++){
-    upRank[i].innerHTML = (parseInt(upRank[i].innerHTML) + n);
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n);
   }
+}
+
+function deepestChild(){
+  
 }
